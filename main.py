f7293a798c55348ca35f9ebef298caf6797fb42c
Gopied <45171626+Gopied@users.noreply.github.com>
2020-10-01 12:47:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
 class Heap(object):
-    """
-    Une heap est une structure de données sous forme d'arbre.
-    https://en.wikipedia.org/wiki/Heap_(data_structure)
-    CETTE CLASSE JUSTE POUR L'AUTOCOMPLETION DE VOTRE EDITEUR
-    NE VOUS EN OCCUPER PAS
-    """
-
+  
     def insert(self, value: int) -> None:
-        """
-        Ajoute une valeur dans l'arbre
-        """
+        
         pass
 
     def find_min(self) -> int:
-        """
-        Retourne la valeur minimum dans l'arbre
-        """
+        
         pass
 
     def delete_min(self) -> int:
@@ -37,34 +27,170 @@ class Heap(object):
         pass
 
 
-class FibonacciHeap(Heap):
-    """
-    Une fibonnaci heap est un arbre permettant de stocker et trier des donnés efficacement
-    https://en.wikipedia.org/wiki/Fibonacci_heap
-    L'implémentation est décrite en anglais : https://en.wikipedia.org/wiki/Fibonacci_heap#Implementation_of_operations
-    et en français : https://fr.wikipedia.org/wiki/Tas_de_Fibonacci#Implémentation_des_opérations
-    """
+class FibonacciHeap:
+    
+    class Node:
+        def __init__(self, data):
+            self.data = data
+            self.parent = self.child = self.left = self.right = None
+            self.degree = 0
+            self.mark = False
 
-    def insert(self, value: int) -> None:
-        """
-        Ajoute une valeur dans l'arbre
-        """
-        pass
+    def iterate(self, head):
+        node = stop = head
+        flag = False
+        while True:
+            if node == stop and flag is True:
+                break
+            elif node == stop:
+                flag = True
+            yield node
+            node = node.right
+    
+    root_list, min_node = None, None
+    
 
-    def find_min(self) -> int:
-        """
-        Retourne la valeur minimum dans l'arbre
-        """
-        pass
+    total_nodes = 0
+  
+    def find_min(self):
+        return self.min_node
+         
+   
+    def extract_min(self):
+        z = self.min_node
+        if z is not None:
+            if z.child is not None:
+              
+                children = [x for x in self.iterate(z.child)]
+                for i in xrange(0, len(children)):
+                    self.merge_with_root_list(children[i])
+                    children[i].parent = None
+            self.remove_from_root_list(z)
+           
+            if z == z.right:
+                self.min_node = self.root_list = None
+            else:
+                self.min_node = z.right
+                self.consolidate()
+            self.total_nodes -= 1
+        return z
+                    
 
-    def delete_min(self) -> int:
-        """
-        Supprime et retourne la valeur minimum dans l'arbre
-        """
-        pass
+    def insert(self, data):
+        n = self.Node(data)
+        n.left = n.right = n
+        self.merge_with_root_list(n)
+        if self.min_node is None or n.data < self.min_node.data:
+            self.min_node = n
+        self.total_nodes += 1
 
-    def merge(self, fibonnaci_heap: Heap) -> None:
-        """
-        Fusionne deux arbres
-        """
-        pass
+    def decrease_key(self, x, k):
+        if k > x.data:
+            return None
+        x.data = k
+        y = x.parent
+        if y is not None and x.data < y.data:
+            self.cut(x, y)
+            self.cascading_cut(y)
+        if x.data < self.min_node.data:
+            self.min_node = x
+            
+    def merge(self, h2):
+        H = FibonacciHeap()
+        H.root_list, H.min_node = self.root_list, self.min_node
+     
+        last = h2.root_list.left
+        h2.root_list.left = H.root_list.left
+        H.root_list.left.right = h2.root_list
+        H.root_list.left = last
+        H.root_list.left.right = H.root_list
+      
+        if h2.min_node.data < H.min_node.data:
+            H.min_node = h2.min_node
+       
+        H.total_nodes = self.total_nodes + h2.total_nodes
+        return H
+      
+    def cut(self, x, y):
+        self.remove_from_child_list(y, x)
+        y.degree -= 1
+        self.merge_with_root_list(x)
+        x.parent = None
+        x.mark = False
+    
+    def cascading_cut(self, y):
+        z = y.parent
+        if z is not None:
+            if y.mark is False:
+                y.mark = True
+            else:
+                self.cut(y, z)
+                self.cascading_cut(z)
+  
+    def consolidate(self):
+        A = [None] * self.total_nodes
+        nodes = [w for w in self.iterate(self.root_list)]
+        for w in xrange(0, len(nodes)):
+            x = nodes[w]
+            d = x.degree
+            while A[d] != None:
+                y = A[d] 
+                if x.data > y.data:
+                    temp = x
+                    x, y = y, temp
+                self.heap_link(y, x)
+                A[d] = None
+                d += 1
+            A[d] = x
+      
+        for i in xrange(0, len(A)):
+            if A[i] is not None:
+                if A[i].data < self.min_node.data:
+                    self.min_node = A[i]
+        
+    
+    def heap_link(self, y, x):
+        self.remove_from_root_list(y)
+        y.left = y.right = y
+        self.merge_with_child_list(x, y)
+        x.degree += 1
+        y.parent = x
+        y.mark = False
+        
+
+    def merge_with_root_list(self, node):
+        if self.root_list is None:
+            self.root_list = node
+        else:
+            node.right = self.root_list.right
+            node.left = self.root_list
+            self.root_list.right.left = node
+            self.root_list.right = node
+            
+ 
+    def merge_with_child_list(self, parent, node):
+        if parent.child is None:
+            parent.child = node
+        else:
+            node.right = parent.child.right
+            node.left = parent.child
+            parent.child.right.left = node
+            parent.child.right = node
+
+    def remove_from_root_list(self, node):
+        if node == self.root_list:
+            self.root_list = node.right
+        node.left.right = node.right
+        node.right.left = node.left
+   
+    def remove_from_child_list(self, parent, node):
+        if parent.child == parent.child.right:
+            parent.child = None
+        elif parent.child == node:
+            parent.child = node.right
+            node.right.parent = parent
+        node.left.right = node.right
+        node.right.left = node.left
+        
+
+f = FibonacciHeap()
